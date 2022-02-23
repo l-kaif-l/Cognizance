911ff036a80b89889fcbf619e0a8540fbda48b13
l-kaif-l <kaifahmed1052004@gmail.com>
2022-02-23 21:13:33 +0530

    Task-4

--- Task4/3.py
+++ Task4/3.py
@@ -4,15 +4,15 @@ table = []
 
 count = int(input("Enter number of students: "))
 
-for i in range(0,3):
+for i in range(0, count):
     print("Enter details of each student in R.No., Name, Marks format: \n")
     table.append([])
-    for j in range(0, count):
+    for j in range(0, 3):
         Enter_details = input()
         table[i].append(Enter_details)
 
 print(table)
-df = pd.DataFrame(table, index=[1, 2, 3], columns=["R.No.", "Name", "Marks"])
+df = pd.DataFrame(table, columns=["R.No.", "Name", "Marks"])
 print(df)
 print(df[1:2])
 
